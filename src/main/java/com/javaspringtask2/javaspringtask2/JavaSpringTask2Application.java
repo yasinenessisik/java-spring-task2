44ffdf199ca9770e3a94eaf43c0129bea1445037
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-04-03 11:52:04 +0300

    Initial commit

---
+++ src/main/java/com/javaspringtask2/javaspringtask2/JavaSpringTask2Application.java
@@ -0,0 +1,13 @@
+package com.javaspringtask2.javaspringtask2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringTask2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaspringtask2/javaspringtask2/JavaSpringTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspringtask2.javaspringtask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
