da7a658883bd53857c1b7a13edda77835fc5e757
yasinenessisik <128436451+yasinenessisik@users.noreply.github.com>
2024-04-07 04:45:42 +0300

    rezervasyon onaylama endpointi ayarlandı.

--- reservation-backend/src/main/java/com/javaspringtask2/javaspringtask2/repository/ReservationRepository.java
+++ reservation-backend/src/main/java/com/javaspringtask2/javaspringtask2/repository/ReservationRepository.java
@@ -1,7 +1,9 @@
 package com.javaspringtask2.javaspringtask2.repository;
 
 import com.javaspringtask2.javaspringtask2.model.Reservation;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,5 +16,11 @@ public interface ReservationRepository extends JpaRepository<Reservation,String>
     public List<Reservation> getReservationByRestaurantnId(@Param("id") String id);
 
     @Query("SELECT SUM(r.reservationNumberOfPeople) from Reservation r where r.restaurant.restaurantId =:id and r.rezervationDate =:date")
-    public Integer getReservationCapacityWithDate(String id, LocalDate date);
+    public Integer getReservationCapacityWithDate(@Param("id") String id, LocalDate date);
+
+    @Modifying
+    @Query("UPDATE Reservation r SET r.confirmed = true WHERE r.reservationId = :reservationId AND r.customer.customerId = :customerId")
+    @Transactional
+    int confirmReservation(@Param("reservationId") String reservationId, @Param("customerId") String customerId);
+
 }
